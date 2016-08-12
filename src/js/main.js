89f7e6a643e4ca0270d475762694ca1996820fe9
alex rodriguez <alex.rodriguez@austintexas.gov>
2016-08-12 13:50:26 -0500

    added mixins and animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,11 @@
-// put your code here
-// remember, you can use modules with browserify
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $(".Hero").css("opacity", 1 - $(window).scrollTop() / $('.Hero').height());
+
+        if($(window).scrollTop() > ($(".Hero").height())/2) {
+          $('.Brand').addClass('on-scroll');
+        } else {
+          $('.Brand').removeClass('on-scroll');
+        }
+    });
+});
